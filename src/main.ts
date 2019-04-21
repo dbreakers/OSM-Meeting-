07d7ddea491b6d8fe4940fa637302f2787cc6ac4
dbreakers <david.breakwell@gmail.com>
2019-04-21 11:27:01 +0100

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './polyfills';
+import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module';
