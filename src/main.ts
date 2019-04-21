d1dd05fde93d80dc466442dccd52a4ed78148e92
dbreakers <david.breakwell@gmail.com>
2019-04-21 11:21:02 +0100

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import './polyfills';
-
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module';
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
