91acbb5bfc053c4bd7e9c947feebe728fc5b1015
dbreakers <david.breakwell@gmail.com>
2019-08-31 12:06:17 +0100

    Popup for Helper

--- src/app/rosta/rosta.component.ts
+++ src/app/rosta/rosta.component.ts
@@ -112,6 +112,22 @@ removehelper(i,j) {
   this.globals.progs[i].items[0].help.splice(j,1);
 }
 
+filterFunction() {
+  var input, filter, ul, li, a, i, div;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    var txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+}
+}
+
 addhelper(i) {
   var obj = {};
 obj["scout"] = "David";
