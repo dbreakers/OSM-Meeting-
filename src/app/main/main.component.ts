fd12886806f0bd35e41ce4b144e14465fd92aef5
dbreakers <david.breakwell@gmail.com>
2019-09-30 10:21:11 +0100

    2 files modified

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -189,6 +189,8 @@ do_progs(p)
   this.globals.progs = p;
   this.globals.loadprogs=true;
   this.globals.loaded.progs = true;
+ 
+
 
 }
 
@@ -223,6 +225,12 @@ do_progs(p)
     
     this.globals.eventsection=this.globals.mysection;
     this.access = this.globals.sectiondata[5].apis.find(i=>i.apiid==41);
+    if (this.globals.sectiondata[4].items.length==0) {
+      this.globals.loaded.progs = true;
+    }
+    if  (this.globals.sectiondata[3].items.length==0) {
+      this.globals.loaded.events = true;
+    }
      }
   }
 }
