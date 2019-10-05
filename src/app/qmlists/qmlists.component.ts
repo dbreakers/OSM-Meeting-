7822f6578cd1785522c1d4d036c17dfb38e7ad43
dbreakers <david.breakwell@gmail.com>
2019-10-05 19:52:15 +0100

    2 files modified

--- src/app/qmlists/qmlists.component.ts
+++ src/app/qmlists/qmlists.component.ts
@@ -17,7 +17,7 @@ import { Globals } from '../globals';
 import * as ons from 'onsenui'
 import * as pdfMake from 'pdfmake/build/pdfmake.js';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
-
+import {  Sortservice } from '../sort';
 
 @Component({
   selector: 'ons-page[qmlist]',
@@ -26,17 +26,27 @@ import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
 })
 
 export class QMListsComponent implements OnInit {
-  
+
+sortable_list = [];
+
   constructor(private _navigator: OnsNavigator,
     private inj: Injector,
+    private sorting: Sortservice,
     private _params: Params, 
     private globals: Globals,
   
     ) { }
-      
+  
         openMenu() {
     this.inj.get(AppComponent).menu.nativeElement.open();
   }
       ngOnInit() {
+      
+       for(var i = 0; i < this.globals.qmlist.length; i++) {
+         this.sortable_list.push(Object.keys(this.globals.qmlist[i].data.rows).map(key => {
+    this.globals.qmlist[i].data.rows[key].id = key; return this.globals.qmlist[i].data.rows[key];}));
+  
+  this.sortable_list.sort(this.sorting.compareValuesArray(["1"],"desc"));
+       }
       }
 }
