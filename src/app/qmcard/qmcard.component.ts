5f9ef6e8552a3f0c0561bcde77d705b3287a868a
dbreakers <david.breakwell@gmail.com>
2019-10-06 17:04:11 +0100

    3 files modified

--- src/app/qmcard/qmcard.component.ts
+++ src/app/qmcard/qmcard.component.ts
@@ -13,6 +13,7 @@ import {
 } from "ngx-onsenui";
 
 import { AppComponent } from "../app.component";
+import {Dropbox} from 'dropbox';
 import { Globals } from "../globals";
 import * as ons from "onsenui";
 import * as pdfMake from "pdfmake/build/pdfmake.js";
@@ -28,6 +29,8 @@ export class QMcardComponent implements OnInit {
   //sortable_list = [];
   QMitem = new Object();
   QMlist = new Object();
+  accessToken = "7hU8qdXO9HAAAAAAAAC8VPwLqOsa_xemA1P5pd6KVRUiDf4svT7fj8UetMDYNw-1" ;
+  //dbx = new Object;
   constructor(
     private _navigator: OnsNavigator,
     private inj: Injector,
@@ -48,5 +51,12 @@ export class QMcardComponent implements OnInit {
         this.QMitem = this.QMlist.data.rows[this._params.data.id];
       }
     }
-  }
+ var dbx = new Dropbox({ accessToken:this.accessToken});
+ dbx.filesListFolder({    path: ''  }).then(response => console.log(response))   
+ dbx.filesGetThumbnailBatch({    entries: [{  path: '"/new haw banner.pdf',size: 'w32h32', format: 'png'}]  
+});
 }
+
+}
+  
+ 
