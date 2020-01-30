93c229509bdf440ace251a80c33ee25a5d693d36
dbreakers <david.breakwell@gmail.com>
2020-01-30 16:55:36 +0000

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -58,7 +58,7 @@ section_data_return(data) {
 }
 
 section_API_return(api) {
-  
+  this.logonService.setAPIvalues2(api);
    this.logonService.getSectionData(this.globals.mysection,this.globals.config[2][this.globals.mysection][this.globals.current_term].termid).subscribe(SectionConfig => this.section_data_return(SectionConfig));
 }
 
